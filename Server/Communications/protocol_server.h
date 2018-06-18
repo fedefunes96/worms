98a67574f7201fdb47161ce883965696ee281521
Fede <fede.funes96@gmail.com>
2018-06-18 04:46:28 -0300

    Delete commented line

--- Server/Communications/protocol_server.h
+++ Server/Communications/protocol_server.h
@@ -15,7 +15,6 @@ public:
     ProtocolServer(Socket& conexion);
 
     void sendPosition(const std::string& type_obj, int32_t id_obj, float posX, float posY, float angle);
-    //void sendActualWorm(int8_t id);
     void sendWormId(int8_t id, int8_t id_worm, int32_t health);
     void sendUsableId(int8_t id, int32_t ammo);
     void sendPlayerId(int8_t id);
